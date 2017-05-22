24cf0dc0b2347ba7242c2b8c21b1648110db314d
Szymon Nieznański <s.nez@member.fsf.org>
2017-05-22 18:15:18 +0200

    Add new exercise to class05

--- class05/README.md
+++ class05/README.md
@@ -1,12 +1,8 @@
 # Warsztat 05
 
 ## Zakres materiału
-* [Wyrażenia regularne - ciąg dalszy](https://github.com/slimakuj/perl/blob/master/class05/lecture.md#wyra%C5%BCenia-regularne---ci%C4%85g-dalszy)
-* [Funkcje ze stanem](https://github.com/slimakuj/perl/blob/master/class05/lecture.md#funkcje-ze-stanem)
-* [Dispatch tables](https://github.com/slimakuj/perl/blob/master/class05/lecture.md#dispatch-table)
-
+* [Parametry interpretera](https://github.com/slimakuj/perl/blob/master/class05/lecture.md#parametry-interpretera)
+* [Bloki BEGIN i END](https://github.com/slimakuj/perl/blob/master/class05/lecture.md#bloki-begin-i-end)
+* [Referencje do funkcji](https://github.com/slimakuj/perl/blob/master/class05/lecture.md#referencje-do-funkcji)
 ## Zadania
-* 01 \- [Wyszukiwanie](https://github.com/slimakuj/perl/blob/master/class05/exercises/ex01-grep.md)
-* 02 \- [Numerowanie tagów](https://github.com/slimakuj/perl/blob/master/class05/exercises/ex02-numbering.md)
-* 03 \- [Dopasowanie](https://github.com/slimakuj/perl/blob/master/class05/exercises/ex03-match.md)
-* 04 \- [Informacje i szablon](https://github.com/slimakuj/perl/blob/master/class05/exercises/ex04-users-template.md)
+* 01 \- [Znajdowanie regionów bogatych w GC/AT](https://github.com/slimakuj/perl/blob/master/class05/exercises/ex01-seq_regions.md)
---
+++ class05/exercises/ex01_seq_regions.pl
@@ -0,0 +1,109 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use feature qw[ say ];
+use autodie;
+use Getopt::Long;
+use Pod::Usage;
+
+my %modes = (
+    at => { A => 1, T => 1, G => 0, C => 0 },
+    gc => { A => 0, T => 0, G => 1, C => 1 },
+);
+my (%arg, $filename);
+{
+    local $SIG{__DIE__} =
+      sub { pod2usage -noperldoc => 1, -verbose => 1, -msg => $_[0] };
+    GetOptions(\%arg, qw/
+        mode=s
+        from=i
+        to=i
+        size=i
+    /) or die 'Invalid arguments';
+
+    foreach my $required (qw[ mode from to size ]) {
+        die "Missing --$required" unless defined $arg{$required};
+    }
+    foreach my $positive (qw[ from to size ]) {
+        die "Invalid --$positive: $arg{$positive}" unless $arg{$positive} > 0;
+    }
+    die "Invalid mode: $arg{mode}" unless exists $modes{ $arg{mode} };
+
+    $filename = shift or die 'No filename specified';
+}
+
+open my $fh, '<', $filename;
+my ($from, $to, $size) = @arg{qw[ from to size ]};
+
+my @buffer;
+my $add_new_entry = do {
+    my $subtotal    = 0;
+    my $num_val_ref = $modes{ $arg{mode} };
+    sub {
+        my ($entry) = @_;
+        push @buffer, $entry;
+        $subtotal += $num_val_ref->{$entry};
+        if (@buffer > $size) {
+            my $value_removed = shift @buffer;
+            $subtotal -= $num_val_ref->{$value_removed};
+        }
+        return if @buffer < $size;    # not enough values yet
+        return $subtotal;
+    };
+};
+
+my $percentage_match = sub {
+    my ($subtotal) = @_;
+    my $percentage = ($subtotal / $size) * 100;
+    return ($from <= $percentage and $percentage <= $to);
+};
+
+while (<$fh>) {
+    chomp;
+
+    foreach my $entry (split //) {
+        my $subtotal = $add_new_entry->($entry);
+        if (defined $subtotal and $percentage_match->($subtotal)) {
+            say join '', @buffer;
+        }
+    }
+}
+
+close $fh;
+
+__END__
+=pod
+
+=head1 NAME
+
+ex01_seq_regions.pl - detect regions with given AT or CG density in a DNA sequence
+
+=head1 SYNOPSIS
+
+Find all regions of size 80 with GC density in 55%-65% range:
+
+  $ perl ex01_seq_regions.pl --from 55 --to 65 --size 80 --mode gc sequence.txt
+
+=head1 ARGUMENTS
+
+=head2 --mode [at|gc]
+
+Specify whether AT or GC density is to be checked.
+
+=head2 --size [INT]
+
+The size of regions to check.
+
+=head2 --from [INT]
+
+Beginning (inclusive) of the desired range.
+
+=head2 --to [INT]
+
+End (inclusive) of the desired range.
+
+=head2 --help
+
+Display this help.
+
+=cut
